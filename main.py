5af78c346f0b3681b174849444405e367908b438
nnastaran83 <nnastaran83@gmail.com>
2022-08-02 02:12:02 +0300

    sec commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from puzzle_2019 import Day1
-
-print(Day1.part_1('input_files/Puzzle2019_Day1_Input.txt'))
-print(Day1.part_2('input_files/Puzzle2019_Day1_Input.txt'))
+from puzzle_2019 import Day2
+# print(Day1.part_1('input_files/Puzzle2019_Day1_Input.txt'))
+# print(Day1.part_2('input_files/Puzzle2019_Day1_Input.txt'))
+print(Day2.part_1('input_files/Puzzle2019_Day2_Input.txt'))
+print(Day2.part_2('input_files/Puzzle2019_Day2_Input.txt'))
