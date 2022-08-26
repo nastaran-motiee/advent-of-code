a6d80b7ef5c4409607765bbe72f73de7766d2786
nnastaran83 <nnastaran83@gmail.com>
2022-08-27 00:57:47 +0300

    Day3_part2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from puzzle_2019 import Day1,Day2,Day3
+from puzzle_2019 import Day1, Day2, Day3
+
+
 def main():
     # print(Day1.part_1('input_files/Puzzle2019_Day1_Input.txt'))
     # print(Day1.part_2('input_files/Puzzle2019_Day1_Input.txt'))
     # print(Day2.part_1('input_files/Puzzle2019_Day2_Input.txt'))
     # print(Day2.part_2('input_files/Puzzle2019_Day2_Input.txt'))
     print(Day3.part_1('input_files/Puzzle2019_Day3_Input.txt'))
+
 
 if __name__ == '__main__':
     main()
--- puzzle_2019/Day3.py
+++ puzzle_2019/Day3.py
@@ -1,80 +1,65 @@
 def part_1(m_input_path):
-    A, B = open(m_input_path).read().split('\n')
-    A, B = [x.split(',') for x in [A,B]]
+    a, b = open(m_input_path).read().split('\n')
+    a, b = [x.split(',') for x in [a, b]]
 
-    A_points = set()
-    B_points=set()
-    
-    A_loc = {'x': 0, 'y': 0}
-    B_loc = {'x': 0, 'y': 0}
+    a_step = 0
+    b_step = 0
 
-    for d in A:
-        direction = d[0]
-        n = int(d[1:])
+    a_points = f_wire_points(a)
+    b_points = f_wire_points(b)
 
-        match direction:
-            case 'R':
-                 for _ in range(n):
-                    A_loc['x'] += 1
-                    A_points.add(tuple(A_loc.values()))
-
-            case 'L':
-                 for _ in range(n):
-                    A_loc['x'] -= 1
-                    A_points.add(tuple(A_loc.values()))        
-
-            case 'U':
-                for _ in range(n):
-                    A_loc['y'] += 1
-                    A_points.add(tuple(A_loc.values()))
-                        
-            case 'D':
-                for _ in range(n):
-                    A_loc['y'] -= 1
-                    A_points.add(tuple(A_loc.values()))  
-
-    for d in B:
-        direction = d[0]
-        n = int(d[1:])
-
-        match direction:
-            case 'R':
-                 for _ in range(n):
-                    B_loc['x'] += 1
-                    B_points.add(tuple(B_loc.values()))
-
-            case 'L':
-                 for _ in range(n):
-                    B_loc['x'] -= 1
-                    B_points.add(tuple(B_loc.values()))       
-
-            case 'U':
-                for _ in range(n):
-                    B_loc['y'] += 1
-                    B_points.add(tuple(B_loc.values()))
-                        
-            case 'D':
-                for _ in range(n):
-                    B_loc['y'] -= 1
-                    B_points.add(tuple(B_loc.values())) 
-            
-    mutual = A_points & B_points
+    mutual = set(a_points) & set(b_points)
     m_distance = float('inf')
+    m_steps = float('inf')
 
     for point in mutual:
-        temp =  abs(point[0]) + abs(point[1])
-        if temp < m_distance:
-            m_distance = temp
-    
-    return m_distance
+        dis = abs(point[0]) + abs(point[1])  # for part 1
+        if dis < m_distance:
+            m_distance = dis
 
-  
+    for point in mutual:
+        t_st = 0
+        for p in a_points:
+            t_st += 1
+            if p == point:
+                break
+        for p in b_points:
+            t_st += 1
+            if p == point:
+                break
 
-            
-       
-        
-       
-            
+        if t_st < m_steps:
+            m_steps = t_st
+
+    return f"part 1: {m_distance} ,part 2: {m_steps}"
 
 
+def f_wire_points(wire):
+    wire_loc = {'x': 0, 'y': 0}
+    wire_points = []
+    for d in wire:
+        direction = d[0]
+        n = int(d[1:])
 
+        match direction:
+            case 'R':
+                for _ in range(n):
+                    wire_loc['x'] += 1
+                    wire_points.append(tuple(wire_loc.values()))
+
+            case 'L':
+                for _ in range(n):
+                    wire_loc['x'] -= 1
+                    wire_points.append(tuple(wire_loc.values()))
+
+            case 'U':
+                for _ in range(n):
+                    wire_loc['y'] += 1
+                    wire_points.append(tuple(wire_loc.values()))
+
+            case 'D':
+                for _ in range(n):
+                    wire_loc['y'] -= 1
+                    wire_points.append(tuple(wire_loc.values()))
+
+    return wire_points
