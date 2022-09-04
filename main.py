0588ba7e3babdc3ab028b4da10ecc481bfc54067
nnastaran83 <nnastaran83@gmail.com>
2022-09-05 02:09:49 +0300

    Day 2 completed

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import puzzle_2018
 from puzzle_2018.Day1 import *
+from puzzle_2018.Day2 import *
 import puzzle_2019
 from puzzle_2019.Day3 import *
 
 
 def main():
-    # print(Day1.part_1('input_files/Puzzle2019_Day1_Input.txt'))
-    # print(Day1.part_2('input_files/Puzzle2019_Day1_Input.txt'))
-    # print(Day2.part_1('input_files/Puzzle2019_Day2_Input.txt'))
-    # print(Day2.part_2('input_files/Puzzle2019_Day2_Input.txt'))
-    # print(Day3.part_1_2('input_files/Puzzle2019_Day3_Input.txt'))
+    # print(puzzle_2019.Day1.part_1('input_files/Puzzle2019_Day1_Input.txt'))
+    # print(puzzle_2019.Day1.part_2('input_files/Puzzle2019_Day1_Input.txt'))
+
+    # print(puzzle_2019.Day2.part_1('input_files/Puzzle2019_Day2_Input.txt'))
+    # print(puzzle_2019.Day2.part_2('input_files/Puzzle2019_Day2_Input.txt'))
+
     # print(puzzle_2019.Day3.part_1_2('input_files/Puzzle2019_Day3_Input.txt'))
-    print(puzzle_2018.Day1.part_1_2('input_files/Puzzle2018_Day1_Input.txt'))
+    # print(puzzle_2019.puzzle_2019.Day3.part_1_2('input_files/Puzzle2019_Day3_Input.txt'))
+    # print(puzzle_2018.Day1.part_1_2('input_files/Puzzle2018_Day1_Input.txt'))
+    print(puzzle_2018.Day2.part_1('input_files/Puzzle_2018_Day2_Input.txt'))
+    print(puzzle_2018.Day2.part_2('input_files/Puzzle_2018_Day2_Input.txt'))
 
 
 if __name__ == '__main__':
